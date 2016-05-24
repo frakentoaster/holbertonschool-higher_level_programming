5f50cf1834c9bf8a406e40320f146719ca016470
Rick Houser <21@p4-5.hbtn.io>
2016-05-24 15:55:08 -0700

    add enumeration

--- manage_the_school_with_swift/person.swift
+++ manage_the_school_with_swift/person.swift
@@ -5,6 +5,13 @@
   Class describes a Person
   Exercise to manipulate Object, Class, Enum and Protocols in Swift
 */
+enum Subject {
+  case Math
+  case English
+  case French
+  case History
+}
+
 protocol Classify {
   func isStudent() -> Bool
 }
@@ -26,6 +33,23 @@ class Person {
 }
 
 class Mentor : Person, Classify {
+  var subject: Subject
+  init(first_name: String, last_name: String, age: Int, subject: Subject = Subject.Math) {
+    self.subject = subject
+    super.init(first_name : first_name, last_name : last_name, age : age)
+  }
+  func stringSubject() -> String {
+    switch subject {
+      case.Math:
+        return("Math")
+      case.English:
+        return("English")
+      case.French:
+        return("French")
+      case.History:
+        return("History")
+    }
+  }
   func isStudent() -> Bool {
     return false
   }
