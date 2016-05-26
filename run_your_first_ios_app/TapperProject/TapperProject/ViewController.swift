1f457285d1d2d825ba35877cbd8182c57f53d45f
Rick Houser <21@p4-5.hbtn.io>
2016-05-25 17:41:08 -0700

    iOS Swift app. Add click button records clicks and updates label with click count

--- run_your_first_ios_app/TapperProject/TapperProject/ViewController.swift
+++ run_your_first_ios_app/TapperProject/TapperProject/ViewController.swift
@@ -23,14 +23,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var textfield_number: UITextField!
     @IBOutlet weak var button_play: UIImageView!
     @IBOutlet weak var image_tapper: UIButton!
+    @IBOutlet weak var label_taps: UILabel!
+    @IBOutlet weak var button_coin: UIButton!
+    var taps_done: Int = 0
+    var taps_requested: Int = 0
 
     // MARK: Actions
-    
     @IBAction func clickPlayButton(sender: AnyObject) {
         let int = Int(textfield_number.text!)!
         if int > 0 {
-            print("Let's do \(int) taps")   
+            print("Let's do \(int) taps")
         }
+    }
+    @IBAction func clickCoinButton(sender: AnyObject) {
+        print("Tap!")
+        taps_done += 1
+        let result = String(taps_done)
+        label_taps.text = (result)
     }
 }
 
