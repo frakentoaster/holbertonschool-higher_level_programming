e81f91e9d898ac2c2bc2e873124bc8c3d0fb1db7
FrackenToaster <frakentoaster@gmail.com>
2016-06-03 11:11:02 -0700

    Add methods to return JSON and return DOM element

--- xml_and_json_are_the_base_of_web_2_0/car.py
+++ xml_and_json_are_the_base_of_web_2_0/car.py
@@ -1,3 +1,4 @@
+import json
 ''' Class '''
 class Car():
 
@@ -47,3 +48,23 @@ class Car():
 	''' Update private attributes '''
 	def set_nb_doors(self, number):
 		self.__nb_doors = number
+
+	''' Return string in JSON format '''
+	def to_json_string(self):
+		struct = { 'name': self.__name,
+				  'brand': self.__brand,
+				  'nb_doors': self.__nb_doors }
+		json_string = json.dumps(struct)
+		return json_string
+
+	''' Return DOM element '''
+	def to_xml_node(self, doc):
+			car = doc.createElement('car')
+			car.setAttribute('nb_doors', str(self.__nb_doors))
+			name = doc.createCDATASection(self.__name)
+			brand = doc.createElement('brand')
+			self_brand = doc.createTextNode(self.__brand)
+			brand.appendChild(self_brand)
+			car.appendChild(name)
+			car.appendChild(brand)
+			return car
