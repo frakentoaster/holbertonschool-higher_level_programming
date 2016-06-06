beb9670bb695874f8acc7e941c143c58aa694657
FrackenToaster <frakentoaster@gmail.com>
2016-06-06 13:48:36 -0700

    Script adds two people to Person table and displays the result

---
+++ basic_sql_playground/2-main.sql
@@ -0,0 +1,4 @@
+/* Script adds two people to Person table and displays result */
+INSERT INTO Person VALUES (7, 'Jon', 'Snow', 26);
+INSERT INTO Person VALUES (8, 'Arya', 'Stark', 12);
+SELECT * FROM Person ORDER BY last_name ASC;
