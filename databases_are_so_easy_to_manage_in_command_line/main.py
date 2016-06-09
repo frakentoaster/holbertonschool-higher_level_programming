8143e1c2286def82cdae8e54dd4e624ccbefc8c4
FrackenToaster <frakentoaster@gmail.com>
2016-06-08 18:25:48 -0700

    Script compares command line args with a set arglist.

---
+++ databases_are_so_easy_to_manage_in_command_line/main.py
@@ -0,0 +1,15 @@
+''' Script checks if a given argv is among the arglist '''
+
+import sys
+from peewee import *
+
+if len(sys.argv) >= 2:
+	arglist = ["create", "print", "insert", "delete"]
+	arg1 = str(sys.argv[1])
+
+	if (sys.argv[1]) in arglist:
+		print str(sys.argv[1])
+	else:
+		print "Undefined action %s" % (arg1)
+else:
+	print "Please enter an action"
